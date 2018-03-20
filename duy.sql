f55bb89fdd7671d2f5f2b75b973c0e554a6ced9c
Do Duy <juchiast@gmail.com>
2018-03-20 07:59:54 +0700

    Update duy.sql

--- duy.sql
+++ duy.sql
@@ -1,2 +1,17 @@
 use QLDT
 go
+
+-- Q12
+select gv.hoten from
+giaovien gv join giaovien gv1 on gv.gvqlcm = gv1.magv
+where gv1.hoten = N'Nguyễn Thanh Tùng'
+
+-- Q18
+select gv1.hoten from
+giaovien gv join giaovien gv1 on gv.mabm = gv1.mabm
+where gv.hoten = N'Trần Trà Hương' and not gv1.hoten = N'Trần Trà Hương'
+
+-- Q24
+select gv1.* from
+giaovien gv join giaovien gv1 on gv.mabm = gv1.mabm
+where gv.magv = '002' and not gv1.magv = '002'
