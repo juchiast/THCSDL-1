71a053e36a2c0059b8d36866a0c11689547300e5
Do Duy <juchiast@gmail.com>
2018-03-20 08:12:31 +0700

    Update duy.sql

--- duy.sql
+++ duy.sql
@@ -11,6 +11,16 @@ select gv1.hoten from
 giaovien gv join giaovien gv1 on gv.mabm = gv1.mabm
 where gv.hoten = N'Trần Trà Hương' and not gv1.hoten = N'Trần Trà Hương'
 
+-- Q19
+select distinct gv.* from
+giaovien gv join bomon bm on gv.magv = bm.truongbm
+join detai on gv.magv = detai.gvcndt
+
+-- Q21
+select distinct gv.hoten from
+giaovien gv join bomon bm on gv.magv = bm.truongbm
+join detai on gv.magv = detai.gvcndt
+
 -- Q24
 select gv1.* from
 giaovien gv join giaovien gv1 on gv.mabm = gv1.mabm
